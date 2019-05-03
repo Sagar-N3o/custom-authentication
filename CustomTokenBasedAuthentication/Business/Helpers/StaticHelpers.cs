96ed5a365361d57ea8f198ccfea1e9f45c0740d6
DeveloperDante <n3o735@gmail.com>
2019-05-03 17:27:39 +0530

    implement login method of auth controller

--- CustomTokenBasedAuthentication/Business/Helpers/StaticHelpers.cs
+++ CustomTokenBasedAuthentication/Business/Helpers/StaticHelpers.cs
@@ -1,20 +1,39 @@
 ﻿using CustomTokenBasedAuthentication.Business.Enums;
 using CustomTokenBasedAuthentication.Business.Models;
+using System.Security.Cryptography;
 
 namespace CustomTokenBasedAuthentication.Business.Helpers
 {
     public static class StaticHelpers
     {
         public static ResponseDetails SetResponSeDetails(bool success, object data, MessageType messageType,
-            string message = "Exception Encountered")
-        {
-            return new ResponseDetails
+            string message = "Exception Encountered") => new ResponseDetails
             {
                 Success = success,
                 Data = data,
                 Message = message,
                 MessageType = messageType
             };
+
+        public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (HMACSHA512 hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (HMACSHA512 hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+
+                for (int i = 0; i < computedHash.Length; i++)
+                    if (computedHash[i] != passwordHash[i]) return false;
+            }
+            return true;
         }
     }
 }
--- CustomTokenBasedAuthentication/Business/Services/AuthService.cs
+++ CustomTokenBasedAuthentication/Business/Services/AuthService.cs
@@ -1,14 +1,9 @@
 ﻿using AutoMapper;
 using CustomTokenBasedAuthentication.Business.Contracts.Repositories;
 using CustomTokenBasedAuthentication.Business.Contracts.Services.Auth;
+using CustomTokenBasedAuthentication.Business.Helpers;
 using CustomTokenBasedAuthentication.Business.Models;
-using CustomTokenBasedAuthentication.Business.Repositories;
 using CustomTokenBasedAuthentication.Database.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Threading.Tasks;
 
 namespace CustomTokenBasedAuthentication.Business.Services
 {
@@ -30,28 +25,15 @@ namespace CustomTokenBasedAuthentication.Business.Services
             if (user == null)
                 return null;
 
-            if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+            if (!StaticHelpers.VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
                 return null;
 
             return _mapper.Map<UserViewModel>(user);
         }
 
-        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
-        {
-            using (var hmac = new HMACSHA512(passwordSalt))
-            {
-                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-
-                for(int i = 0; i < computedHash.Length; i++)
-                    if (computedHash[i] != passwordHash[i]) return false;
-            }
-            return true;
-        }
-
         public UserViewModel Register(UserViewModel user, string password)
         {
-            byte[] passwordHash, passwordSalt;
-            CreatePasswordHash(password, out passwordHash, out passwordSalt);
+            StaticHelpers.CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
 
             User model = _mapper.Map<User>(user);
 
@@ -62,15 +44,6 @@ namespace CustomTokenBasedAuthentication.Business.Services
             _unitOfWork.SaveChanges();
 
             return _mapper.Map<UserViewModel>(model);
-        }
-
-        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
-        {
-            using (var hmac = new HMACSHA512())
-            {
-                passwordSalt = hmac.Key;
-                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-            }
         }
     }
 }
